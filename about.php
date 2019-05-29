e14b521466c9874cbe7393451ab88ec018a702a6
Teklay <teklay2108@gmail.com>
2019-05-28 23:17:47 -0900

    2 files created

---
+++ about.php
@@ -0,0 +1,35 @@
+<html>
+<head></head>
+<style type="text/css">
+	body
+	{
+		background-color: lightgray;
+	}
+	.div1
+	{
+		background-color: #A1B455;
+		text-align: center;
+		margin-left: 25%;
+		width: 50%;
+	}
+	.div1 a
+	{
+		border: 2px solid white;
+		color: green;
+		font-size: 50px;
+		background-color: yellow;
+
+	}
+</style>
+<body>
+	<div class="div1">
+		<h1>Wellcome to this website!</h1>
+		<p>This website developed using github desktop</p>
+		<a href="home.php">Home</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="abou.php">About Us</a>
+	</div>
+	<div>
+		<h2>This is about us!</h2>
+	</div>
+</body>
+</html>
