c8aa5297e1ddd1574dffde6f66351c3123b2a7c9
Teklay <teklay2108@gmail.com>
2019-05-28 23:13:02 -0900

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,21 @@
 		margin-left: 25%;
 		width: 50%;
 	}
+	.div1 a
+	{
+		border: 2px solid white;
+		color: green;
+		font-size: 50px;
+		background-color: yellow;
+
+	}
 </style>
 <body>
 	<div class="div1">
 		<h1>Wellcome to this website!</h1>
 		<p>This website developed using github desktop</p>
+		<a href="home.php">Home</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="abou.php">About Us</a>
 	</div>
 </body>
 </html>
